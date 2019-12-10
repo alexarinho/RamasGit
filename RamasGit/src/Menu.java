9323f098930572f5fa2bd5645d9e436092af9516
alumno <alumno@aula1-11>
2019-12-10 11:12:28 +0100

    Menu creado

---
+++ RamasGit/src/Menu.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @author Alex
+ * @version 1.0
+ *
+ */
+public class Menu {
+	
+	/**
+	 * Método que pinta un menú en pantalla
+	 */
+	public static void pintaMenu() {
+		System.out.println("------Menu-------");
+	}
+}
